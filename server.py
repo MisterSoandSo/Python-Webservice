8cd1a1909c9e1cc8c9b5149cabd0349e5d91f426
Andrew So <asoso221@gmail.com>
2023-10-01 19:02:32 -0700

    Implement Rate Limiting for route calls

--- server.py
+++ server.py
@@ -1,14 +1,24 @@
-from flask import Flask
+from flask import Flask, request
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
+
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    storage_uri="memory://",
+)
 
 @app.route('/status', methods=['GET'])
 def a_msg():
     return "Alive!"
 
 @app.route('/predict', methods=['GET'])
+@limiter.limit("5/minute")  # Allow 5 requests per minute
 def b_msg():
-    return "Prediction!"
+    return "Predictionroute is rate-limited!"
 
 
-app.run(debug=True, port = 5000)
+if __name__ == '__main__':
+    app.run(debug=True, port = 5000)
